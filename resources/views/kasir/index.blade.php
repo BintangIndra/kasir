07f936d09b47e96e5815be2e86c7579111259dc1
Bintang Indra Wijaya <bintangindra.lol@gmail.com>
2022-06-24 17:56:44 +0700

    done menu

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -21,11 +21,12 @@
 
             <div class="border border-info col-sm-12 d-flex justify-content-between" style="height: 25% !important;">
                 <div class="ms-1">
-                    <p>Total :</p>
-                    <p id="totalBayar" data-total="0">0</p>
+                    <p style="margin:0 !important">Total :</p>
+                    <p id="totalBayar" data-total="0" style="margin:0 !important">0</p>
                     <p>Diskon :</p>
+                    <input type="number" class="form-control">
                 </div>
-                <button type="submit" class="btn btn-info">Simpan</button>
+                <button type="submit" class="btn btn-info" onclick="">Simpan</button>
             </div>
 
         </div>
@@ -33,9 +34,9 @@
 
     <script>
         dataCategory(true);
-        dataCard('makanan');
+        dataCard('all');
         
-        var content = '<div><button class="btn btn-info m-1" onclick="dataCard(\'all\')">Semua</button>';
+        var content = '<div class="col-sm-12"><button class="btn btn-info m-1" onclick="dataCard(\'all\')">Semua</button>';
 
         function dataCategory(data){
             $.ajax({
@@ -64,10 +65,10 @@
                     var cardcontent = '';
                     $.each(data, function( index, value ) {
                         cardcontent +=
-                        "<div class='card col-sm-2 m-2' onclick='addMenu("+JSON.stringify(value)+")'>"+
-                        "    <img src='{{asset("images")}}/"+value.imageUrl+"' class='card-img-top' alt='"+value.nama+"'>"+
-                        "    <div class='card-body'>"+
-                        "       <p class='card-text'>"+value.nama+"</p>"+
+                        "<div class='card col-sm-2 m-2' style='height:20% !important;' onclick='addMenu("+JSON.stringify(value)+")'>"+
+                        "    <img src='{{asset("images")}}/"+value.imageUrl+"' class='card-img-top img-fluid'>"+
+                        "    <div class='card-body' style='padding:0px !important;'>"+
+                        "       <p class='card-text text-truncate'>"+value.nama+"</p>"+
                         "    </div>"+
                         "</div>";
                     });
@@ -77,16 +78,20 @@
             });
         };
 
+        var menu=[];
         function addMenu(value){
             var jumlah = 1;
             if($('#idmenu'+value.id).length != 0){
                 jumlah = $('#idmenu'+value.id).data("jumlah") + 1;
                 $('#idmenu'+value.id).remove();
-                $('#datalist').append("<li class='list-group-item' id='idmenu"+value.id+"' data-jumlah='"+jumlah+"' onclick='removeMenu("+JSON.stringify(value)+")'><p>"+value.nama+" = "+jumlah+" X "+value.harga+"</p></li>");
+                
+                var indx=menu.findIndex(x => x.id === value.id);
+                menu[indx] ={id: value.id,nama : value.nama,harga : value.harga,jumlah : jumlah};
             }else{
-                $('#datalist').append("<li class='list-group-item' id='idmenu"+value.id+"' data-jumlah='1' onclick='removeMenu("+JSON.stringify(value)+")'><p>"+value.nama+" = "+jumlah+" X "+value.harga+"</p></li>");
+                menu.push({id: value.id,nama : value.nama,harga : value.harga,jumlah : jumlah});
             }
 
+            drawmenu();
             $('#totalBayar').html($('#totalBayar').data("total") + value.harga);
             $('#totalBayar').data("total",$('#totalBayar').data("total") + value.harga);
         }
@@ -96,13 +101,25 @@
             if($('#idmenu'+value.id).data("jumlah") > 1){
                 jumlah = $('#idmenu'+value.id).data("jumlah") - 1;
                 $('#idmenu'+value.id).remove();
-                $('#datalist').append("<li class='list-group-item' id='idmenu"+value.id+"' data-jumlah='"+jumlah+"' onclick='removeMenu("+JSON.stringify(value)+")'><p>"+value.nama+" = "+jumlah+" X "+value.harga+"</p></li>");
+                
+                var indx=menu.findIndex(x => x.id === value.id);
+                menu[indx] ={id: value.id,nama : value.nama,harga : value.harga,jumlah : jumlah};
             }else{
+                menu.splice(menu.findIndex(x => x.id === value.id), 1);
                 $('#idmenu'+value.id).remove();
             }
 
+            drawmenu();
             $('#totalBayar').html($('#totalBayar').data("total") - value.harga);
             $('#totalBayar').data("total",$('#totalBayar').data("total") - value.harga);
+        }
+
+        function drawmenu(){
+            var listMenu = '';
+            $.each(menu, function( index, value ) {
+                listMenu += "<li class='list-group-item' id='idmenu"+value.id+"' data-jumlah='"+value.jumlah+"' onclick='removeMenu("+JSON.stringify(value)+")'><p>"+value.nama+" = "+value.jumlah+" X "+value.harga+"</p></li>";
+            });
+            $('#datalist').html(listMenu);
         }
 
     </script>
