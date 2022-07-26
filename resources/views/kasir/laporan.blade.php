77939e4a75e4c2b5b9eef96ca56f8d60f813f287
Bintang Indra Wijaya <bintangindra.lol@gmail.com>
2022-07-26 16:08:33 +0700

    Update laporan.blade.php

--- resources/views/kasir/laporan.blade.php
+++ resources/views/kasir/laporan.blade.php
@@ -16,7 +16,7 @@
               <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true" onclick="renderDataPenjualan()">Data Penjualan</button>
             </li>
             <li class="nav-item" role="presentation">
-              <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Laporan Penjualan</button>
+              <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false" onclick="renderLaporanPenjualan()">Laporan Penjualan</button>
             </li>
             <li class="nav-item" role="presentation">
               <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Contact</button>
@@ -76,6 +76,50 @@
 
             <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
                 
+                <div class="d-flex justify-content-start mb-2 mt-2">
+                    <button class="btn btn-info" type="button" data-bs-toggle="collapse" data-bs-target="#advancedSearchLaporanPenjualan" aria-expanded="false" aria-controls="advancedSearchLaporanPenjualan">
+                        Advanced Search
+                    </button>
+                </div>
+                <div class="collapse mb-2" id="advancedSearchLaporanPenjualan">
+                    <div class="card card-body" style="background-color: #04293A;">
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <label for="dateTransAS">Tanggal Transaksi</label>
+                                <input type="date" id="dateTransAS" class="form-control">
+                            </div>
+                            <div class="col-sm-2">
+                                <label for="atasNamaAS">Atas Nama</label>
+                                <input type="text" id="atasNamaAS" class="form-control">
+                            </div>
+                            <div class="col-sm-2">
+                                <label for="totalAS">Total</label>
+                                <input type="number" id="totalAS" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <table id="laporanPenjualan" class="table table-dark" style="width:100%;">
+                    <thead>
+                        <tr>
+                            <th>ID Transaksi</th>
+                            <th>Atas Nama</th>
+                            <th>Tanggal transaksi</th>
+                            <th>Total</th>
+                            <th style="max-width: 20px !important;">Action</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr id="tfootLaporanPenjualan">
+                            <td></td>
+                            <td></td>
+                            <td>Total</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tfoot>
+                </table>
+
             </div>
 
             
@@ -97,6 +141,8 @@
 
         var tableDataPenjualan;
         var totalDataPenjualan = 0;
+
+        var tableLaporanPenjualan;
 
         var modal = $('#modalview').html();
 
@@ -144,7 +190,6 @@
                 drawTableDataPenjualan();
             }
         }
-
 
         function drawTableDataPenjualan() {
             totalDataPenjualan = 0;
@@ -258,6 +303,62 @@
                     $('#Modalview').modal('show');
                 }
             });
+        }
+
+        function renderLaporanPenjualan() {
+            if (isInstanceDatatableCL('#laporanPenjualan')) {
+                tableLaporanPenjualan.destroy();
+                drawTableLaporanPenjualan();
+            } else {
+                drawTableLaporanPenjualan();
+            }
+        }
+
+        function drawTableLaporanPenjualan() {
+            tableLaporanPenjualan = $('#laporanPenjualan').DataTable({
+            paging: true,
+            ajax: {
+                url: "{{ route('kasir.show') }}",
+                data:function(params) {
+                    params.status = 0;
+                },
+                dataSrc: function ( responses ) {
+                    return responses;
+                },
+                complete:function(){
+                    $( tableLaporanPenjualan.column( 3 ).footer() ).html(
+                        totalDataPenjualan
+                    );
+                }
+            },
+            columns: [
+                { data: 'idTransaksi' },
+                { data: 'atasNama' },
+                { data: 'created_at' },
+                { data: 'count',class: "text-end"},
+                {
+                    data: "id",
+                    class: "text-end",
+                    render: function ( data, type, row, meta ) {
+                        let action =
+                        '<ul class="navbar-nav">'+
+                        '    <li class="nav-item dropdown">'+
+                        '        <a class="btn btn-info dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">'+
+                        '        </a>'+
+                        '        <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">'+
+                        '        <button type="button" class="dropdown-item" onclick="detailDataPenjualan('+meta.row+')">View</button>'+
+                        '    </li>'+
+                        '</ul>'
+                        ;
+
+                        return action;
+                    }
+                },
+            ],
+            "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+                totalDataPenjualan += parseInt(aData.count);
+            },
+        });
         }
 
     </script>
